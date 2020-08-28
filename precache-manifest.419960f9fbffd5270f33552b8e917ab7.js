6dcd6ccf303398473d71d8f34cb0ae645a05ccbd
Darren Jian <darrenjian1@gmail.com>
2020-08-28 11:45:45 -0700

    Updates

---
+++ precache-manifest.419960f9fbffd5270f33552b8e917ab7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad63670ec9ce8e037dc0b4179ad82fd9",
+    "url": "/planty/index.html"
+  },
+  {
+    "revision": "c6893edba2221282ecd5",
+    "url": "/planty/static/css/2.e5db91d3.chunk.css"
+  },
+  {
+    "revision": "d669b2ce9f8142982667",
+    "url": "/planty/static/css/main.ff3c4616.chunk.css"
+  },
+  {
+    "revision": "c6893edba2221282ecd5",
+    "url": "/planty/static/js/2.0c7ff5bf.chunk.js"
+  },
+  {
+    "revision": "d22059b534b59d0853f884796b3dbead",
+    "url": "/planty/static/js/2.0c7ff5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d669b2ce9f8142982667",
+    "url": "/planty/static/js/main.7835c037.chunk.js"
+  },
+  {
+    "revision": "6e5b965f22aa339a1b87",
+    "url": "/planty/static/js/runtime-main.07e41b64.js"
+  },
+  {
+    "revision": "e793a57f8b59422fab2b6eddd1549ab9",
+    "url": "/planty/static/media/LeafyIcon.e793a57f.png"
+  },
+  {
+    "revision": "75ec810c0786c64dc74862fa15078230",
+    "url": "/planty/static/media/Untitled_Artwork 6.75ec810c.png"
+  }
+]);
